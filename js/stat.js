cca1b562380d4e0d50ec01b2a670441c5df90cd8
svdunaev <54572104+svdunaev@users.noreply.github.com>
2020-03-10 15:31:15 +0500

    немного белого окна

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,15 @@
 'use strict';
 
-window.renderStatistics = function(ctx) {
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 50, 500, 200);
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function (ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx){
+    renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
+    renderCloud(ctx, 100, 50, '#fff');
+
 };
