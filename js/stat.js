04a5a741bc11dc35af4d4c09351fd45ef2de585c
svdunaev <54572104+svdunaev@users.noreply.github.com>
2020-03-10 15:52:49 +0500

    форматирует

--- js/stat.js
+++ js/stat.js
@@ -4,12 +4,12 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
 var renderCloud = function (ctx, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx){
-    renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
-    renderCloud(ctx, 100, 50, '#fff');
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, 100, 50, '#fff');
 
 };
